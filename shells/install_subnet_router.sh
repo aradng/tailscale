e4b80cc1d746f2e90231a000cf5c602b58ff3151
aradng <aradarang@gmail.com>
2024-07-23 18:45:10 +0330

    [feat] colorize logs

--- shells/install_subnet_router.sh
+++ shells/install_subnet_router.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# colors
+NC='\033[0m'       # Text Reset
+Green='\033[1;32m'       # Green
+Red='\033[1;31m'         # Red
+Blue='\033[0;34m'         # Blue
+
 # install iptables-persistant
 echo iptables-persistent iptables-persistent/autosave_v4 boolean false | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean false | sudo debconf-set-selections
@@ -8,7 +14,7 @@ apt update && apt-get -y install iptables-persistent ipcalc ipset
 if [ -f "$(dirname "$0")/../.env" ]; then
   export $(grep -v '^#' "$(dirname "$0")/../.env" | xargs)
 else
-  echo "Error: .env file not foupnd."
+  echo -e "${RED}Error: .env file not foupnd.${NC}"
   exit 1
 fi
 
@@ -21,17 +27,30 @@ check_and_amend_rules() {
     for rule in "${rules[@]}"; do
         # Check if the rule exists
         if iptables -C ${rule} >/dev/null 2>&1; then
-            echo "Rule ${rule} already exists."
+            echo -e "${Blue}Rule ${rule} already exists.${NC}"
         else
             # Amend the rule when it doesn't exist
             iptables -A ${rule}
-            echo "Rule ${rule} has been added."
+            echo -e "${Green}Rule ${rule} has been added.${NC}"
         fi
     done
 }
 
 # Extract the SERVICES variable, removing the square brackets
 SERVICES=$(echo $SERVICES | sed -E 's/\[|\]//g')
+
+IFS=',' # Set Internal Field Separator to comma
+# Iterate over each service in the SERVICES variable
+for service in $SERVICES; do
+  # Split the service into protocol and port
+  PROTOCOL=$(echo "$service" | cut -d':' -f1)
+  PORT=$(echo "$service" | cut -d':' -f2)
+
+  # Add the rule to allow packets out for secondary subnet/ ignore primary subnet (tailscale will handle it)
+  rules_to_check+=("allow-outgoing -t mangle -p $PROTOCOL ! -s $PRIMARY_SUBNET --sport $PORT -j MARK --set-xmark 0x80000")
+#   rules_to_check+=("allow-outgoing -t mangle -p $PROTOCOL -d $PRIMARY_SUBNET --sport $PORT -j MARK --set-xmark 0x80000")
+done
+IFS=$' \t\n'
 
 # masquerade egress for local subnet and allow public access to machine while using exit-node
 ipset create bypass nethash
@@ -43,19 +62,9 @@ rules_to_check=(
     # set mss to 1200 for tailscale0 mtu cap of 1280
     "FORWARD -t mangle -o tailscale0 -p tcp -m tcp --tcp-flags SYN,RST SYN -j TCPMSS --set-mss 1200"
 )
-# Iterate over each service in the SERVICES variable
-
-for service in $SERVICES; do
-  # Split the service into protocol and port
-  PROTOCOL=$(echo "$service" | cut -d':' -f1)
-  PORT=$(echo "$service" | cut -d':' -f2)
-
-  # Add the rule to allow packets out for secondary subnet/ ignore primary subnet (tailscale will handle it)
-  rules_to_check+=("allow-outgoing -t mangle -p $PROTOCOL ! -s $PRIMARY_SUBNET --sport $PORT -j MARK --set-xmark 0x80000")
-#   rules_to_check+=("allow-outgoing -t mangle -p $PROTOCOL -d $PRIMARY_SUBNET --sport $PORT -j MARK --set-xmark 0x80000")
-done
 
 check_and_amend_rules "${rules_to_check[@]}"
+echo -e "${Green} iptables rules have been added.${NC}"
 
 # iptables/ipset persistence
 cp services/ipset-persistent.service /etc/systemd/system
@@ -65,12 +74,13 @@ ip6tables-save > /etc/iptables/rules.v6
 ipset save > /etc/iptables/ipset
 systemctl enable ipset-persistent.service
 # systemctl enable ipset-fetch.service
-
+echo -e "${Green} iptables/ipset persistence has been enabled.${NC}"
 
 # enable packet forwarding
 echo 'net.ipv4.ip_forward = 1' > /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' >> /etc/sysctl.d/99-tailscale.conf
 sysctl -p /etc/sysctl.d/99-tailscale.conf
+echo -e "${Green} Packet forwarding has been enabled.${NC}"
 
 # custom netplan for tailscale compatibility
 cp ./configs/tailscale_config.yaml .
@@ -83,6 +93,7 @@ sed -i "s|PRIMARY_CIDR|$PRIMARY_CIDR|g" tailscale_config.yaml
 sed -i "s|PRIMARY_GATEWAY|$PRIMARY_GATEWAY|g" tailscale_config.yaml
 mv tailscale_config.yaml /etc/netplan
 chmod 600 /etc/netplan/tailscale_config.yaml
+echo -e "${Green} Custom netplan configuration has been added.${NC}"
 netplan try
 
-echo "tailscale up --login-server=https://headscale.com --advertise-route=$PRIMARY_SUBNET,$SECONDARY_SUBNET --exit-node-allow-lan-access --accept-routes"
+echo -e "${Blue}tailscale up --login-server=https://headscale.com --advertise-route=$PRIMARY_SUBNET,$SECONDARY_SUBNET --exit-node-allow-lan-access --accept-routes${NC}"
